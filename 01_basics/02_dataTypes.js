987d78e421f8fad4e9415bc079e6920a69714d62
Omeshwar Deshmukh <127861743+OmeshwarDeshmukh@users.noreply.github.com>
2024-02-04 05:54:58 +0000

    Data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as a newer version
+
+// alert(3+3) -> it gives error because we use node, not browser.
+
+// Data types
+
+// premitive data types
+
+let name = "Oma"  // string
+let age = 19  // number (range 2^53)
+let isLoggedIn = false   // boolean
+let state   // undefined
+let num = null  // null (stand alone value)
+let badget = 837483748893882874837892748748398947398498397478398474n  //bigInt
+let temp = undefined  // value is not assinged
+// symbol -> unique
+
+// object
+
+console.log(typeof null)   // Object
+console.log(typeof undefined)    // undefined
+
