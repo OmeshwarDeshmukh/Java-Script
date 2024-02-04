493c5f8ae501e8634684884c273f5e1d1ac40621
Omeshwar Deshmukh <127861743+OmeshwarDeshmukh@users.noreply.github.com>
2024-02-04 12:47:03 +0000

    Objects in depth.

--- 01_basics/04_objects.js
+++ 01_basics/04_objects.js
@@ -41,9 +41,63 @@ JsUser.greetingsTwo = function() {
     console.log(`Hello, ${this.name}!`)
 }
 
-JsUser.greetings()
-JsUser.greetingsTwo()
+// JsUser.greetings()
+// JsUser.greetingsTwo()
 
 // 02 contractor -> singleton
 
 // Object.create
+
+const tinderUser = new Object()
+
+tinderUser.name = "oma"
+tinderUser.location = "nagpur"
+tinderUser.id = 937
+tinderUser.age = 20
+
+// console.log(tinderUser)
+
+const regularUser = {
+    id: 2933,
+    fullName : {
+        firstName: "oma",
+        lastName: "deshmukh"
+    }
+}
+
+// console.log(regularUser.fullName.firstName)
+
+const obj1 = {1: "a", 2:"b"}
+const obj2 = {3: "c", 4:"d"}
+const obj4 = {5: "e", 6:"f"}
+
+// const obj3 = { obj1, obj2}  <- wrong way
+// const obj3 = Object.assign({}, obj1, obj2, obj4) <- rarelly used
+const obj3 = {...obj1, ...obj2, ...obj4}
+
+// console.log(obj3)
+
+const users = [
+    one = {
+        name: "oma",
+        age: 20
+    },
+
+    two = {
+        name: "niku",
+        age: 20
+    }, 
+
+    three = {
+        name: "kuldya",
+        age: 16
+    }
+]
+
+// console.log(users[1].name)
+
+// console.log(tinderUser)
+// console.log(Object.keys(tinderUser))
+// console.log(Object.values(tinderUser))
+// console.log(Object.entries(tinderUser))
+console.log(tinderUser.hasOwnProperty("age"))
